033da54b9492a64ba9a24b88517b61f12b3b59f8
Aayush-2219 <aayush.raj2211@gmail.com>
2025-08-03 11:57:05 +0530

    Add deploy files

--- App.py
+++ App.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-if __name__=="__main__":
-    app.run(port=8051)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
